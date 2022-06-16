b07e723022306d8844fd2caafb22129a83c31968
Lara R <lararabanoh@gmail.com>
2022-06-16 23:05:22 +0200

    Creación del juego

---
+++ arkanoid/game.py
@@ -0,0 +1,24 @@
+import pygame
+
+from arkanoid import ALTO, ANCHO
+
+class Arkanoid:
+    def __init__(self) -> None:
+        print("Arranca el juego!!")
+        pygame.init()
+        self.display = pygame.display.set_mode((ANCHO, ALTO))
+    
+    def jugar(self):
+        """""Este es el bucle principal"""
+        salir = False
+        while not salir:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    salir = True
+            self.display.fill((99,99,99))
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    juego = Arkanoid()
+    juego.jugar()
+
