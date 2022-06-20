712f0d5cff9b9e785709cec54bf602ff5a8f72fa
Lara R <lararabanoh@gmail.com>
2022-06-20 23:45:02 +0200

    Cosas

--- arkanoid/escenas.py
+++ arkanoid/escenas.py
@@ -1,4 +1,12 @@
+"""Esto me permite que la ruta de llamada de los archivos coloque las barras automáticamente en función del sistema 
+operativo en el que esté. Así mi juego funciona en cualquier sistema operativo"""
+
+from curses.ascii import alt
+import os 
+
 import pygame as pg
+
+from . import ANCHO, ALTO #He puesto el punto porque Escenas está en arkanoid. Esto es una "importación relativa"
 
 class Escena:
    
@@ -12,16 +20,27 @@ class Escena:
       """
 
 class Portada(Escena):
+
+    def __init__(self, pantalla: pg.Surface): #Le digo que pantalla es de tipo surface
+        super().__init__(pantalla)
+        self.logo = pg.image.load(os.path.join("Resources", "Images", "arkanoid_name.png"))
      
-     def bucle_principal(self):
+    def bucle_principal(self):
         salir = False
         while not salir:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     salir = True
+
             self.pantalla.fill((0,99,0))
+            self.pintar_logo()
             pg.display.flip()
 
+    def pintar_logo(self):
+        ancho_logo = self.logo.get_width()
+        pos_x = (ANCHO - ancho_logo)/2
+        pos_y = ALTO/3
+        self.pantalla.blit(self.logo,(pos_x, pos_y))
 
 class Partida(Escena):
     
@@ -33,6 +52,8 @@ class Partida(Escena):
                     salir = True
             self.pantalla.fill((0,0,99))
             pg.display.flip()
+
+            
 
 class HallofFame(Escena):
     
